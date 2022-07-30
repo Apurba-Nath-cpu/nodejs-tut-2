2586a6d147405e5b33ee1397984a31627302a143
Apurba-Nath-cpu <apurba20542@iiitkalyani.ac.in>
2022-07-31 04:23:23 +0530

    eighth commit

--- 6-alternative-flavor.js
+++ 6-alternative-flavor.js
@@ -1,6 +1,6 @@
-module.exports.items = ['item1', 'item2', 'item3'];
+module.exports.items = ['item1', 'item2', 'item3']; // alternate-1
 const person = {
     name: 'bob',
 }
 
-module.exports.singlePerson = person;
+module.exports.singlePerson = person; // alternate-2
